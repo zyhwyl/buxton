a6b091fee397545c9749fe627d1c9ad424d78767
zyhwyl <zyh19880605@gmail.com>
2014-03-10 22:50:00 +0800

    实现了滚动显示分页信息的功能

--- beta_0.5/main.js
+++ beta_0.5/main.js
@@ -2,6 +2,9 @@
 
 $(function(){
   var tmp=[];
+  var now_page_cache=[];
+  var now_page_position=0;
+  var SCROLL_DIRECTION="down";
   var awesomeThings = [
     'HTML5 Boilerplate',
     'AngularJS',
@@ -25,36 +28,76 @@ $(function(){
   var nowPage=pageHelper.getNowPageNo();
   pageHelper.initPageSelect(".pageNo");
 
-  for (var i = 0; i < nowData.length; i++) {
-    $("#list").append("<li>"+nowData[i]+"</li>");
-  }
+  now_page_cache.push(appendContent($("#list"),nowData,nowPage));
 
   //scroll load
   var scroll_lock=false;
-  var basic_top=$("#list").offset().top;
+  var base_top=$("#list").offset().top;
   var page_height=$("#list").height();
-  $(window).scroll(function () {
-    c($(window).scrollTop());
-      /*判断窗体高度与竖向滚动位移大小相加 是否 超过内容页高度*/
-      if (($(window).height() + $(window).scrollTop()) >= $("body").height()){
-        if(!scroll_lock){
-          scroll_lock=true;
-          setTimeout(function(){
-            scroll_lock=false;
-            $(".gotoNext").click();
-          },1000);
+  var winH = $(window).height(); //页面可视区域高度  
+  var pageH = $(document.body).height(); //页面总高度 
+  var scrollT = $(window).scrollTop(); //滚动条top  
+  $(window).scroll(function(){
+    var base_position=$(window).scrollTop()-base_top;
+    if(now_page_position>$(window).scrollTop()){
+      SCROLL_DIRECTION="up";
+    }else{
+      SCROLL_DIRECTION="down";
+    }
+    now_page_position=$(window).scrollTop();
+
+    if(SCROLL_DIRECTION==="down"){
+      for (var i = 0; i < now_page_cache.length; i++) {
+        if(nowPage==now_page_cache[i].pageNo){
+          page_height=now_page_cache[i].pageTop;
         }
+      };
+      //if now position > this page content`s height,page number++
+      if(base_position>page_height){
+        nowPage++;
       }
+    }else{
+      for (var i = 0; i < now_page_cache.length; i++) {
+        if(nowPage==now_page_cache[i].pageNo){
+          if(i-1>=0){
+            page_height=now_page_cache[i-1].pageTop;
+            //get the height of pre page,and if the base_position < the height of now page,page number-- 
+            if(base_position<page_height){
+              nowPage--;
+            }
+          }
+        }
+      };
+    }
+    
+    $("#pageNo").text(nowPage);
+    var aa = (pageH-winH-scrollT)/winH; 
+    if(aa<0.01){
+      if(!scroll_lock){
+        scroll_lock=true;
+        setTimeout(function(){
+          scroll_lock=false;
+          var _data=pageHelper.goNextPage();
+          now_page_cache.push(appendContent($("#list"),_data,pageHelper.getNowPageNo()));
+        },1000);
+      }
+    }
   });
-  c(basic_top)
 });
 
 function c(obj){
   console.log(obj);
 }
 
-function refreshContent($wrap){
-
+function appendContent($wrap,$data,$pageNo){
+  var _ulc=$("<ul/>");
+  for (var i = 0; i < $data.length; i++) {
+    _ulc.append("<li>"+$data[i]+"</li>");
+  }
+  _ulc.attr("page-no",$pageNo);
+  $wrap.append(_ulc);
+  _ulc.attr("top",_ulc.offset().top+_ulc.height());
+  return {pageNo:$pageNo,pageTop:_ulc.offset().top+_ulc.height()};
 }
 
 var ScrollListener=(function(){
