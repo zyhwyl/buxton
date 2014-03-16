2d0f8ce284f4bbdfc82ad2e6a962e87c794a8624
zyhwyl <zyh19880605@gmail.com>
2014-03-16 22:42:13 +0800

    完成了大部分功能

--- beta_0.5/main.js
+++ beta_0.5/main.js
@@ -1,10 +1,12 @@
 'use strict';
+var tmp=[];
+var now_page_cache=[];
+var now_page_position=0;
+var nowPage=1;
+var SCROLL_DIRECTION="down";
+var scroll_lock=false;
 
 $(function(){
-  var tmp=[];
-  var now_page_cache=[];
-  var now_page_position=0;
-  var SCROLL_DIRECTION="down";
   var awesomeThings = [
     'HTML5 Boilerplate',
     'AngularJS',
@@ -24,41 +26,49 @@ $(function(){
   }
 
   var pageHelper=new PageHelper(tmp);
-  var nowData=pageHelper.getNowDataList();
-  var nowPage=pageHelper.getNowPageNo();
+  nowPage=pageHelper.getNowPageNo();
   pageHelper.initPageSelect(".pageNo");
 
-  now_page_cache.push(appendContent($("#list"),nowData,nowPage));
+  freshContent($("#list"),pageHelper.getNowDataList(),nowPage);
 
   //scroll load
-  var scroll_lock=false;
   var base_top=$("#list").offset().top;
   var page_height=$("#list").height();
-  var winH = $(window).height(); //页面可视区域高度  
-  var pageH = $(document.body).height(); //页面总高度 
-  var scrollT = $(window).scrollTop(); //滚动条top  
+  
   $(window).scroll(function(){
+    var winH = $(window).height(); //页面可视区域高度  
+    var pageH = $(document.body).height(); //页面总高度 
+    var scrollT = $(window).scrollTop(); //滚动条top  
     var base_position=$(window).scrollTop()-base_top;
     if(now_page_position>$(window).scrollTop()){
       SCROLL_DIRECTION="up";
     }else{
       SCROLL_DIRECTION="down";
     }
+
     now_page_position=$(window).scrollTop();
 
     if(SCROLL_DIRECTION==="down"){
       for (var i = 0; i < now_page_cache.length; i++) {
         if(nowPage==now_page_cache[i].pageNo){
-          page_height=now_page_cache[i].pageTop;
+          page_height=now_page_cache[i].pageTop-now_page_cache[i].pageTop%100;
         }
       };
+      c(page_height)
       //if now position > this page content`s height,page number++
       if(base_position>page_height){
         nowPage++;
       }
     }else{
       for (var i = 0; i < now_page_cache.length; i++) {
+        //向上滚动时 检查出上一页是否存在
         if(nowPage==now_page_cache[i].pageNo){
+          if(nowPage!=1){
+            //上一页不存在 加载上一页
+            if(now_page_cache[i].pageNo-1!=now_page_cache[i-1].pageNo){
+              scrollChangePage(pageHelper,"pre");
+            }
+          }
           if(i-1>=0){
             page_height=now_page_cache[i-1].pageTop;
             //get the height of pre page,and if the base_position < the height of now page,page number-- 
@@ -70,34 +80,82 @@ $(function(){
       };
     }
     
-    $("#pageNo").text(nowPage);
-    var aa = (pageH-winH-scrollT)/winH; 
+    $(".pageNoBtn").removeClass("font-weight");
+    $("#pageNoBtn"+nowPage).addClass("font-weight");
+
+    var aa = (pageH-window.innerHeight-scrollT)/winH;
     if(aa<0.01){
-      if(!scroll_lock){
-        scroll_lock=true;
-        setTimeout(function(){
-          scroll_lock=false;
-          var _data=pageHelper.goNextPage();
-          now_page_cache.push(appendContent($("#list"),_data,pageHelper.getNowPageNo()));
-        },1000);
-      }
+      scrollChangePage(pageHelper,"next");
     }
   });
 });
+
+function scrollChangePage($pageHelper,$direction){
+  if(!scroll_lock){
+    scroll_lock=true;
+    setTimeout(function(){
+      scroll_lock=false;
+      var _data;
+      if($direction=="next")
+        _data=$pageHelper.goNextPage();
+      else
+        _data=$pageHelper.goPrePage();
+      var _now_scroll_top=$(window).scrollTop();
+      freshContent($("#list"),_data,$pageHelper.getNowPageNo());
+      $(window).scrollTop(_now_scroll_top);
+    },1000);
+  }
+}
 
 function c(obj){
   console.log(obj);
 }
 
-function appendContent($wrap,$data,$pageNo){
-  var _ulc=$("<ul/>");
-  for (var i = 0; i < $data.length; i++) {
-    _ulc.append("<li>"+$data[i]+"</li>");
+function freshContent($wrap,$data,$pageNo,$goto){
+  var is_cache_exist=false;
+  //检查跳转的页面是否已经存在
+  for (var i = 0; i < now_page_cache.length; i++) {
+    if(now_page_cache[i].pageNo==$pageNo)
+      is_cache_exist=true;
   }
-  _ulc.attr("page-no",$pageNo);
-  $wrap.append(_ulc);
-  _ulc.attr("top",_ulc.offset().top+_ulc.height());
-  return {pageNo:$pageNo,pageTop:_ulc.offset().top+_ulc.height()};
+  if(!is_cache_exist){
+    $wrap.html("");
+    now_page_cache = sortCache(now_page_cache,$pageNo,$data);
+    for (var i = 0; i < now_page_cache.length; i++) {
+      var _ulc=$("<ul/>");
+      for (var j = 0; j < now_page_cache[i].pageData.length; j++) {
+        _ulc.append("<li>"+now_page_cache[i].pageData[j]+"</li>");
+      }
+      _ulc.append("<li style='text-align:right;padding:5px;list-style:none;'>"+now_page_cache[i].pageNo+"</li>");
+      _ulc.attr("page-no",now_page_cache[i].pageNo);
+      $wrap.append(_ulc);
+      _ulc.attr("top",_ulc.offset().top+_ulc.height());
+      now_page_cache[i].pageTop=_ulc.offset().top+_ulc.height();
+    };
+  }
+  if($goto){
+    $(window).scrollTop(now_page_cache[now_page_cache.length-2].pageTop);
+    nowPage=$pageNo;
+  }
+  return true;
+}
+
+function sortCache($cache_list,$pageNo,$data){
+  var _insert_flag=false;
+  if($cache_list.length==0){
+    $cache_list.push({pageNo:$pageNo,pageData:$data});
+  }else{
+    for (var i = 0; i < $cache_list.length; i++) {
+      if($pageNo<$cache_list[i].pageNo){
+        $cache_list.splice(i-1,0,{pageNo:$pageNo,pageData:$data});
+        _insert_flag=true;
+      }
+    };
+    if(!_insert_flag){
+      $cache_list.push({pageNo:$pageNo,pageData:$data});
+    }
+  }
+  return $cache_list;
 }
 
 var ScrollListener=(function(){
@@ -151,8 +209,8 @@ PageHelper.prototype={
         $('<span class="pageNoBtn" id="pageNoBtn'+i+'" rel="'+i+'">'+i+'</span>').appendTo($wrap);
         $("#pageNoBtn"+i).click(function(){
           var tmpId=$(this).attr("rel");
-          $this.pageNo=tmpId;
-          $("#gotoBtn").click();
+          $this.gotoPage(tmpId);
+          freshContent($("#list"),$this.getNowDataList(),$this.getNowPageNo(),true);
         });
       };
       $('<span class="pageNoBtn">...</span>').appendTo($wrap);
